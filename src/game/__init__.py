ff69542309710a730908efc397200c4115aaf28e
Willy Samuel Paz Colque <samuelpaz243@gmail.com>
2022-12-12 10:00:09 -0400

    [ADD] Flask server and socketio config

--- src/game/__init__.py
+++
@@ -1,5 +0,0 @@
-from room import Room
-
-class Game:
-    def __init__(self, player) -> None:
-        self.room: Room = Room(player)
--- src/game/room.py
+++
@@ -1,8 +0,0 @@
-class Room:
-    def __init__(self, player) -> None:
-        self.owner = player
-        pass
-    def join_room(self, player, code):
-        pass
-    def leave_room(self, player):
-        pass
